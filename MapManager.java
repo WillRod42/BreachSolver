b67aed0abf54e9ab7050bc041c52393043e761e5
William Rodriguez <william@rodriguezfamily.tv>
2018-03-08 15:52:26 -0800

    MapManager able to store and print map of level.

--- MapManager.java
+++ MapManager.java
@@ -1,3 +1,29 @@
+import java.util.*;
+
 public class MapManager {
+   private String[][] map;
    
+   public MapManager(Scanner readMap) {
+      map = new String[8][8];
+      for(int i = 7; i >= 0; i--) {
+         String line = readMap.nextLine();
+         Scanner readLine = new Scanner(line);
+         for(int l = 0; l < 8; l++) {
+            map[i][l] = readLine.next();
+         }
+      }
+   }
+   
+   public MapManager(String[][] map) {
+      this.map = map;
+   }
+   
+   public void printMap() {
+      for(int i = 7; i >= 0; i--) {
+         for(int l = 0; l < 8; l++) {
+            System.out.print(map[i][l] + "   ");
+         }
+         System.out.println("\n");
+      }
+   }
 }
--- SolverMain.java
+++ SolverMain.java
@@ -1,5 +1,53 @@
+import java.util.*;
+import java.io.*;
+
 public class SolverMain {
-   public static void main(String[] args) {
+   private static MapManager manager;
+   private static Scanner console;
    
+   public static final String VERSION = "0.0.2";
+   
+   public static void main(String[] args) throws FileNotFoundException {
+      console = new Scanner(System.in);
+   
+      System.out.println("Breach Solver " + VERSION);
+      System.out.print("Create(c) or load(l) map?: ");
+      
+      if(console.next().equals("c")) {
+         manager = buildMap();
+      } else {
+         System.out.print("Input file name: ");
+         manager = new MapManager(new Scanner(new File(console.next())));
+      }
+      
+      System.out.println();
+      manager.printMap();
+   }
+   
+   public static MapManager buildMap() {
+      String[][] map = new String[8][8];
+   
+      System.out.println("---Build Map---");
+      for(int i = 0; i < 8; i++) {
+         for(int l = 0; l < 8; l++) {
+            map[i][l] = getString("Tile (" + i + "," + l + "): ");
+         }
+      }
+      System.out.println("---------------");
+      return new MapManager(map);
+   }
+   
+   private static String getString(String prompt) {
+      System.out.print(prompt);
+      String input = console.next();
+      
+      while(!input.equals("g") && !input.equals("w") && !input.equals("b") && 
+            !input.equals("e") && !input.equals("m") && !input.equals("p")) {
+         System.out.println("***ERROR: Invalid tile type***\n");
+         System.out.print(prompt);
+         input = console.next();
+      }
+      
+      return input;
    }
 }
